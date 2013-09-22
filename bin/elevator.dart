0e07aed8677e5d017f95f5cd5c3c74417a83eaf0
Eric Taix <eric.taix@gmail.com>
2013-09-22 18:01:30 +0200

    First working algo

--- bin/elevator.dart
+++ bin/elevator.dart
@@ -1,19 +1,106 @@
-import 'dart:io';
-import 'dart:json' as JSON;
+part of elevator;
 
-main() {
-  var port = int.parse(Platform.environment['PORT']);
-  HttpServer.bind('0.0.0.0', port).then((HttpServer server) {
-    print('Server started on port: ${port}');
-    server.listen((HttpRequest request) {
-      var resp = JSON.stringify({
-        'Name': 'Taix',
-        'FirstName': 'Eric',
-        'Environment': Platform.environment}
-      );
-      request.response..headers.set(HttpHeaders.CONTENT_TYPE, 'application/json')
-                      ..write(resp)
-                      ..close();
-    });
-  });
+/// An elevator
+class Elevator {
+  // Internal floor of the elevator. This attribut can't be changed from outside
+  int _floor = 0;
+  // Internal state which reprensents if the door is open
+  bool _doorOpen = false;
+  // The current direction of the elevator
+  Direction _direction = Direction.UP;
+  // List of stops (up and down directions)
+  Map<Direction, Set<int>> _stops = new Map();
+  
+  int get floor => _floor;
+  
+  /// Returns the current stops in the current direction
+  Set<int> get currentStops => _stops[_direction];
+  /// Returns the current stops in the current direction
+  Set<int> get oppositeStops => _stops[_direction.not()];
+  
+  Elevator() {
+    _stops[Direction.UP]= new Set(); 
+    _stops[Direction.DOWN]= new Set(); 
+    reset();
+  }
+  
+  /**
+   * Someone call for the elevator. Add the requested stop according to the direction only
+   * [newFloor] The requested floor
+   * [direction] The requested direction
+   */
+  void call(int newFloor, Direction direction) {
+    go(newFloor);
+  }
+  
+  /**
+   *  Add a new stop. A new stop is added according to the current floor and the direction of the elevator
+   *  [newFloor] The new floor to stop
+   */
+  void go(int newFloor) {
+    if (newFloor < _floor) {
+      _stops[Direction.DOWN].add(newFloor);
+    } else if (newFloor > _floor) {
+      _stops[Direction.UP].add(newFloor);
+    } else {
+      _stops[_direction].add(_floor);
+    }
+  }
+  
+  /**
+   * Returns true if the current floor is the expected floor
+   */
+  bool _expectedFloor(int floor) => floor == _floor;
+  
+  String _openDoor() {
+    _doorOpen = true;
+    return 'OPEN';
+  }
+  
+  String _closeDoor() {
+    _doorOpen = false;
+    return 'CLOSE';
+  }
+  
+  /**
+   *  Return the next command
+   */
+  String nextCommand() {
+    // First do we have some stop at the current floor ?
+    List<int> stops = currentStops.where(_expectedFloor).toList();
+    if (!stops.isEmpty) {
+      currentStops.removeWhere(_expectedFloor);
+      if (!_doorOpen) {
+        return _openDoor();
+      }
+    }
+    // If no stop here, then verify if the door is closed
+    if (_doorOpen) {
+      return _closeDoor();
+    }
+    // If there's more stop in the current direction the continue to move
+    if (!currentStops.isEmpty) {
+      _floor += _direction == Direction.UP ? 1 : -1;
+      return _direction.value;
+    }
+    // Ok no more stop in this direction, try in the opposite direction
+    Direction o = _direction.not();
+    if (!_stops[_direction.not()].isEmpty) {
+      _direction = _direction.not();
+      return nextCommand();
+    }
+    return 'NOTHING';
+  }
+  
+  /**
+   * Reset every internal states
+   */
+  void reset() {
+    _floor =0;
+    _direction = Direction.UP;
+    _doorOpen = false;
+    _stops.forEach((dir, Set set) => set.clear());
+  }
+  
+  String toString() => 'Floor ${_floor}, Direction ${_direction}, CurrentStops ${currentStops}, OppositeStop ${oppositeStops}';
 }
